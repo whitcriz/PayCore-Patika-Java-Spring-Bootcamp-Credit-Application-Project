9c3f1cc08e58c6076a76d7ed35ad3100f4b82c36
Ümmü Gülsüm <u.gulsumkarabudak@gmail.com>
2022-02-24 05:23:37 +0300

    initialize project with base files

---
+++ credit_application/src/test/java/com/paycore/patika/credit_application_system/CreditApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paycore.patika.credit_application_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
