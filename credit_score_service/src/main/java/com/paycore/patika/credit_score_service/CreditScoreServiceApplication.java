d4f81bb77cab16f8aa1dd493623e51dc8605b747
Ümmü Gülsüm <u.gulsumkarabudak@gmail.com>
2022-02-25 21:42:09 +0300

    Credit Score Service Project Base initialized

---
+++ credit_score_service/src/main/java/com/paycore/patika/credit_score_service/CreditScoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paycore.patika.credit_score_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditScoreServiceApplication.class, args);
+	}
+
+}
---
+++ credit_score_service/src/test/java/com/paycore/patika/credit_score_service/CreditScoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paycore.patika.credit_score_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
