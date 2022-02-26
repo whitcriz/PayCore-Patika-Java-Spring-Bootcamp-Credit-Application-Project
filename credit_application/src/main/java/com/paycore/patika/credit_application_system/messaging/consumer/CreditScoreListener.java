db1ae9dc9adac5c35da63095f464629e202ceb0a
Ümmü Gülsüm <u.gulsumkarabudak@gmail.com>
2022-02-26 17:55:09 +0300

    rename and rewrite publisher and listener class to send customer DTO and receive it with an updated credit score

--- credit_application/src/main/java/com/paycore/patika/credit_application_system/messaging/consumer/CreditScoreListener.java
+++ credit_application/src/main/java/com/paycore/patika/credit_application_system/messaging/consumer/CreditScoreListener.java
@@ -1,10 +1,9 @@
 package com.paycore.patika.credit_application_system.messaging.consumer;
 
 import com.paycore.patika.credit_application_system.config.RabbitMQConfig;
-import com.paycore.patika.credit_application_system.model.CustomerRabbitMqDTO;
-import com.paycore.patika.credit_application_system.model.entity.CreditApplication;
+import com.paycore.patika.credit_application_system.model.RabbitCustomerDTO;
 import com.paycore.patika.credit_application_system.model.entity.Customer;
-import com.paycore.patika.credit_application_system.model.mapper.CustomerRabbitMapper;
+import com.paycore.patika.credit_application_system.model.mapper.RabbitCustomerMapper;
 import com.paycore.patika.credit_application_system.service.CreditApplicationService;
 import com.paycore.patika.credit_application_system.service.CustomerService;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +22,14 @@ public class CreditScoreListener {
     @Autowired
     private CustomerService customerService;
 
+    private static final RabbitCustomerMapper RABBIT_CUSTOMER_MAPPER = Mappers.getMapper(RabbitCustomerMapper.class);
+
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE)
-    public void consumeMessageFromQueue(CustomerRabbitMqDTO customerRabbitMqDTO) {
-        Customer customer = CustomerRabbitMapper.toEntityRabbit(customerRabbitMqDTO);
-        CreditApplication creditApplication = customerRabbitMqDTO.getCreditApplication();
-        customerService.updateCustomer(customer);
-        creditApplicationService.updateCreditApplication(creditApplication);
+    public void consumeMessageFromQueue(RabbitCustomerDTO rabbitCustomerDTO) {
+        Customer updatedCustomer = customerService.updateCustomer(RABBIT_CUSTOMER_MAPPER.toEntity(rabbitCustomerDTO));
+        creditApplicationService.UpdateNotResultedApplication(updatedCustomer);
+        System.out.println("Successfully received credit score and updated customer & credit application");
     }
 
 }
---
+++ credit_application/src/main/java/com/paycore/patika/credit_application_system/messaging/producer/CreditApplicationProducer.java
@@ -0,0 +1,31 @@
+package com.paycore.patika.credit_application_system.messaging.producer;
+
+import com.paycore.patika.credit_application_system.config.RabbitMQConfig;
+import com.paycore.patika.credit_application_system.model.entity.Customer;
+import com.paycore.patika.credit_application_system.model.mapper.RabbitCustomerMapper;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/messaging/publish")
+public class CreditApplicationProducer {
+
+
+    @Autowired
+    private RabbitTemplate template;
+
+    private static final RabbitCustomerMapper RABBIT_CUSTOMER_MAPPER = Mappers.getMapper(RabbitCustomerMapper.class);
+
+
+    @PostMapping("/customer")
+    public String publishCustomer(Customer customer) {
+        template.convertAndSend(RabbitMQConfig.EXCHANGE, RabbitMQConfig.ROUTING_KEY, RABBIT_CUSTOMER_MAPPER.toDto(customer));
+        return "Successfully created application";
+    }
+}
