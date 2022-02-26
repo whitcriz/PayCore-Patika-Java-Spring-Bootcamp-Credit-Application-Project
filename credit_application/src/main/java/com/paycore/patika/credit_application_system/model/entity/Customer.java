7e476bc72561bcb936cf4e341c4df954cb04dff6
Ümmü Gülsüm <u.gulsumkarabudak@gmail.com>
2022-02-26 18:12:17 +0300

    remove generated id and replace national identity number with id, change relationship with credit application from OneToOne to OneToMany

--- credit_application/src/main/java/com/paycore/patika/credit_application_system/model/entity/Customer.java
+++ credit_application/src/main/java/com/paycore/patika/credit_application_system/model/entity/Customer.java
@@ -1,33 +1,25 @@
 package com.paycore.patika.credit_application_system.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 import static javax.persistence.FetchType.LAZY;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Validated
 @Entity
 @Table(name = "customer")
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false, nullable = false)
-    private Integer id;
-
-    @Column(name = "national_identity_number", updatable = false, nullable = false)
+    @Column(name = "national_identity_number",length = 11,updatable = false, nullable = false)
     @NotBlank(message = "national identity number can not be blank")
-    @Size(min = 11, max = 11)
-    @JsonFormat(pattern = "[1-9][0-9]{10}")
+    @Pattern(regexp = "[1-9][0-9]{10}")
     private String nationalIdentityNumber;
 
     @NotBlank(message = "name can not be null")
@@ -36,7 +28,8 @@ public class Customer {
     @NotBlank(message = "surname can not be null")
     private String surname;
 
-    @NotBlank(message = "monthly income can not be null")
+    @NotNull(message = "monthly income can not be null")
+    @Min(1)
     @Column(name = "monthly_income")
     private Double monthlyIncome;
 
@@ -51,9 +44,10 @@ public class Customer {
     @Email
     private String email;
 
+    @Transient
     private Integer creditScore;
 
-    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL, fetch = LAZY)
-    private CreditApplication creditApplication;
+    @OneToMany(fetch = LAZY, mappedBy = "customer", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CreditApplication> creditApplications;
 
 }
