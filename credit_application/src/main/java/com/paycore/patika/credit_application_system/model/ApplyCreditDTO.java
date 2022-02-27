9db1c1672bc7653b9dcb8da76d88ad7f90293e7e
Ümmü Gülsüm <u.gulsumkarabudak@gmail.com>
2022-02-27 22:35:16 +0300

    delete 3 files to change some fields

--- credit_application/src/main/java/com/paycore/patika/credit_application_system/model/ApplyCreditDTO.java
+++
@@ -1,9 +0,0 @@
-package com.paycore.patika.credit_application_system.model;
-
-import com.paycore.patika.credit_application_system.model.entity.Customer;
-import lombok.Data;
-
-@Data
-public class ApplyCreditDTO {
-    private Customer customer;
-}
--- credit_application/src/main/java/com/paycore/patika/credit_application_system/model/entity/Resulted.java
+++
@@ -1,7 +0,0 @@
-package com.paycore.patika.credit_application_system.model.entity;
-
-public enum Resulted {
-
-    YES,
-    NO
-}
